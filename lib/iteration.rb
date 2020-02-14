9a3db184ffe44f6fee583b65531195797b5b1d0c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 02:22:11 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -5,13 +5,24 @@ def join_ingredients(src)
   # inserted into this template:
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  # As such, there should be a new String for each inner array, or pair\
+  retArray = []
+  src.length.times do |index_i|
+      retArray.push("I love #{src[index_i][0]} and #{src[index_i][1]} on my pizza")
+  end
+  return retArray
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  
+  retArray =[]
+  src.length.times do |index_i|
+      retArray.push(src[index_i][0]>src[index_i][1]?src[index_i][0]:src[index_i][1])
+  end
+  return retArray
 end
 
 def total_even_pairs(src)
@@ -22,4 +33,9 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  retVal = 0
+  src.length.times do |index_i|
+      retVal += (src[index_i][0]%2 == 0 && src[index_i][1]%2 == 0 ? src[index_i][0]+src[index_i][1]: 0)
+  end
+  return retVal
 end
